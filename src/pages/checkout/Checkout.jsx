c524a82131741e3017a957f7be68eda845786025
smartrove <olaokunolalekan@gmail.com>
2023-07-08 22:16:57 +0100

    update

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -35,17 +35,20 @@ const Checkout = () => {
   useEffect(() => {
     // http://localhost:4242/create-payment-intent
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:4242/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        items: cartItems,
-        userEmail: email,
-        shipping: shippingAddress,
-        billing: billingAddress,
-        description,
-      }),
-    })
+    fetch(
+      "https://smartrovemart-ecommerce.onrender.com/create-payment-intent",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          items: cartItems,
+          userEmail: email,
+          shipping: shippingAddress,
+          billing: billingAddress,
+          description,
+        }),
+      }
+    )
       .then((res) => {
         if (res.ok) {
           return res.json();
